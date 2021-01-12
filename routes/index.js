f37f6df2a9e80f22a1ecee05a9f7f0aa5f3d83be
metalstormbass <michaelbraunbass@gmail.com>
2021-01-11 21:04:46 -0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const request = require('node-fetch');
+const { json } = require('body-parser');
 
 // Variables
 
 var loginUrlBase = 'https://rep.checkpoint.com/rep-auth/service/v1.0/request';
-var apiKey;
-var urlBase = 'https://rep.checkpoint.com/url-rep/service/v2.0/query?resource=';
 
 // Login Function
 
 function cpLogin(apiKey) {
-
+    var authToken;
     var headers = {
         'Client-Key': apiKey,
     };
@@ -22,29 +21,56 @@ function cpLogin(apiKey) {
             headers: headers
         })
         .then(res => res.text())
-        .then(text => authToken = text)
-      
-        console.log(authToken)
-        return authToken;
+        .then(text => authToken = text);
+         return(authToken);
 }
+
+// Post Function
+
+function cpPost(apiKey, authToken, targetUrl) {
+    // console.log(targetUrl)
+    var headers = {
+        'Client-Key': apiKey,
+        'token': authToken
+    };
+    var inputBody = {
+            request: [{
+                resource: targetUrl
+            }]       
+      };
+    var body = JSON.stringify(inputBody);
+    var urlBase = 'https://rep.checkpoint.com/url-rep/service/v2.0/query?resource=' + targetUrl;
+    request(urlBase,
+        {
+            method: 'POST',
+            headers: headers,
+            body: body
+        })
+        //.then(res => console.log(res))
+        //.then(json => output = json)
+        //.then(json => console.log(output))
+        return (json.body);     
+
+}
+
+
 
 // Get home page
 router.get('/', function (req, res, next) {
     res.render('index', { status: null, error: null });
 });
 
-router.post('/', function (req, res) {
+// Parse User Input
+router.post('/', async (req, res) => {
     // Login to CP Reputation
     var apiKey = req.body.apiKey;
-    console.log('testing flow 1');
-    var authToken = cpLogin(apiKey);
-    console.log(authToken)
-    res.render('index', { status: apiKey, error: null });
-
-    // Assemble Request
-   // let targetUrl = req.body.targetUrl;
-    //let apiUrl = urlBase + '${targetUrl}'
-    //Parse response from Post and display it to webpage
+    var targetUrl = req.body.targetUrl;
+    // Call async function
+    var authToken = await cpLogin(apiKey); 
+    await console.log(authToken); 
+    var output = await cpPost(apiKey, authToken, targetUrl); 
+    res.render('index', { status: output, error: null });
+    //console.log(output)
 });  
 
 module.exports = router;
